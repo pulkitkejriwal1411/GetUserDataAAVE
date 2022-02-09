91f3fcbf33b3deb385892949b6df047521c775f0
pk1411 <pulkit.kejriwal@crio-users.in>
2022-02-09 15:48:37 +0530

    added getUserAssetData

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -15,3 +15,20 @@ interface LendingPool{
     );
 
 }
+
+interface AaveProtocolDataProvider {
+    function getUserReserveData(address asset, address user)
+        external
+        view
+        returns (
+            uint256 currentATokenBalance,
+            uint256 currentStableDebt,
+            uint256 currentVariableDebt,
+            uint256 principalStableDebt,
+            uint256 scaledVariableDebt,
+            uint256 stableBorrowRate,
+            uint256 liquidityRate,
+            uint40 stableRateLastUpdated,
+            bool usageAsCollateralEnabled
+        );
+}
--- contracts/resolver.sol
+++ contracts/resolver.sol
@@ -11,6 +11,9 @@ contract Resolver {
     LendingPool LendingPoolObject = LendingPool(LENDING_POOL_ADDRESS);
 
 
+    address PROTOCOL_DATA_PROVIDER_ADDRESS = 0xFA3bD19110d986c5e5E9DD5F69362d05035D045B;
+    AaveProtocolDataProvider ProtocolDataProviderObj = AaveProtocolDataProvider(PROTOCOL_DATA_PROVIDER_ADDRESS);
+
     struct userData{
         uint256 totalCollateralETH;
         uint256 totalDebtETH;
@@ -45,4 +48,62 @@ contract Resolver {
     }
 
 
+    struct UserAsset{
+        uint256 currentATokenBalance;
+        uint256 currentStableDebt;
+        uint256 currentVariableDebt;
+        uint256 principalStableDebt;
+        uint256 scaledVariableDebt;
+        uint256 stableBorrowRate;
+        uint256 liquidityRate;
+        uint40 stableRateLastUpdated;
+        bool usageAsCollateralEnabled;
+    }
+    function GetUserAssetData(address _user,address _asset) external view returns(
+        uint256 currentATokenBalance,
+        uint256 currentStableDebt,
+        uint256 currentVariableDebt,
+        uint256 principalStableDebt,
+        uint256 scaledVariableDebt,
+        uint256 stableBorrowRate,
+        uint256 liquidityRate,
+        uint40 stableRateLastUpdated,
+        bool usageAsCollateralEnabled
+    ){
+        UserAsset memory Usas;
+        (
+        Usas.currentATokenBalance,
+        Usas.currentStableDebt,
+        Usas.currentVariableDebt,
+        Usas.principalStableDebt,
+        Usas.scaledVariableDebt,
+        Usas.stableBorrowRate,
+        Usas.liquidityRate,
+        ,
+        
+        ) = ProtocolDataProviderObj.getUserReserveData(_asset,_user);
+        (
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            ,
+            Usas.stableRateLastUpdated,
+            Usas.usageAsCollateralEnabled
+        ) = ProtocolDataProviderObj.getUserReserveData(_asset,_user);
+
+        return (
+            Usas.currentATokenBalance,
+        Usas.currentStableDebt,
+        Usas.currentVariableDebt,
+        Usas.principalStableDebt,
+        Usas.scaledVariableDebt,
+        Usas.stableBorrowRate,
+        Usas.liquidityRate,
+        Usas.stableRateLastUpdated,
+        Usas.usageAsCollateralEnabled
+        );
+    }
 }
