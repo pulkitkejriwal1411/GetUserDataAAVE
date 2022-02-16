4854b078e0af2d46142884619f8b0d0ecf4ab94a
pk1411 <pulkit.kejriwal@crio-users.in>
2022-02-16 22:24:58 +0530

    using BogNumber

--- scripts/GetUserPosition.js
+++ scripts/GetUserPosition.js
@@ -1,8 +1,9 @@
 //const { inputToConfig } = require("@ethereum-waffle/compiler");
-const { BN } = require("bn.js");
+
 const { expect, assert } = require("chai");
 const { Address } = require("ethereumjs-util");
 const { ethers } = require("hardhat");
+const BigNumber = require("bignumber.js");
 
 const userAddress = "0x51571107Cb5c25b3Ef36B714CBAd17F6F900B936";
 
@@ -17,47 +18,85 @@ async function GetUserPosition() {
 
   let userData = [];
   const ud = await resolveUserPosition.getUserData(userAddress);
-  userData.totalSupply = mod1ex(ud.totalCollateralUSD.toString(),26)+' USD';
-  userData.totalBorrows = mod1ex(ud.totalDebtUSD.toString(),26)+' USD';
-  userData.availableBorrows = mod1ex(ud.availableBorrowsUSD.toString(),26)+' USD';
-  userData.ltv = mod1ex(ud.ltv.toString(),2)+' %';
-  userData.liquidationThreshold = mod1ex(ud.currentLiquidationThreshold.toString(),2)+' %';
-  userData.healthFactor = mod1ex(ud.healthFactor.toString(),18);
+  userData.totalSupply = new BigNumber(ud.totalCollateralUSD.toString());
+  userData.totalSupply = userData.totalSupply.div(1e26) + " USD";
+
+  userData.totalBorrows = new BigNumber(ud.totalDebtUSD.toString());
+  userData.totalBorrows = userData.totalBorrows.div(1e26) + " USD";
+
+  userData.availableBorrows = new BigNumber(ud.availableBorrowsUSD.toString());
+  userData.availableBorrows = userData.availableBorrows.div(1e26) + " USD";
+
+  userData.ltv = Number(ud.ltv) / 100 + " %";
+
+  userData.liquidationThreshold =
+    Number(ud.currentLiquidationThreshold) / 100 + " %";
+
+  userData.healthFactor = new BigNumber(ud.healthFactor.toString());
+  userData.healthFactor = userData.healthFactor.div(1e18).toString();
+
   console.log(userData);
+
   const allTokens = await resolveUserPosition.getAllReservesTokens();
-  for(let i=0;i<allTokens.length;i++)
-  {
+
+  for (let i = 0; i < allTokens.length; i++) {
     let reserveData = [];
     reserveData.symbol = allTokens[i].symbol;
     const reserveAddress = allTokens[i].tokenAddress;
-    const reserveDataNonSpecific = await resolveUserPosition.dataSpecificToReserve(reserveAddress);
-    const reserveDataSpecific = await resolveUserPosition.getTokenDataSpecificToUser(reserveAddress,userAddress);
-    reserveData.supplyRate = mod1ex(reserveDataNonSpecific.supplyRate.toString(),27);
-    reserveData.borrowRate = mod1ex(reserveDataNonSpecific.borrowRate.toString(),27);
-    reserveData.ltv = mod1ex(reserveDataNonSpecific.ltv.toString(),2)+"%";
-    reserveData.liquidationThreshold = mod1ex(reserveDataNonSpecific.liquidationThreshold.toString(),4);
-    reserveData.priceInETH = mod1ex(reserveDataNonSpecific.priceInETH.toString(),18);
-    reserveData.priceInUSD = mod1ex(reserveDataNonSpecific.priceInUSD.toString(),26);
-    reserveData.supply = mod1ex(reserveDataSpecific.supply.toString(),18);
-    reserveData.borrow = mod1ex(reserveDataSpecific.borrow.toString(),18);
-    reserveData.maxBorrowLimit = mod1ex(reserveDataSpecific.maxBorrowLimit.toString(),10);
-    reserveData.maxLiquidationBorrowLimit = mod1ex(reserveDataSpecific.maxLiquidationBorrowLimit.toString(),14);
+    const reserveDataNonSpecific =
+      await resolveUserPosition.dataSpecificToReserve(reserveAddress);
+    const reserveDataSpecific =
+      await resolveUserPosition.getTokenDataSpecificToUser(
+        reserveAddress,
+        userAddress
+      );
+
+    reserveData.supplyRate = new BigNumber(
+      reserveDataNonSpecific.supplyRate.toString()
+    );
+    reserveData.supplyRate = reserveData.supplyRate.div(1e27).toString();
+
+    reserveData.borrowRate = new BigNumber(
+      reserveDataNonSpecific.borrowRate.toString()
+    );
+    reserveData.borrowRate = reserveData.borrowRate.div(1e27).toString();
+
+    reserveData.ltv = Number(reserveDataNonSpecific.ltv) / 100 + "%";
+
+    reserveData.liquidationThreshold =
+      Number(reserveDataNonSpecific.liquidationThreshold.toString()) / 10000;
+
+    reserveData.priceInETH = new BigNumber(
+      reserveDataNonSpecific.priceInETH.toString()
+    );
+    reserveData.priceInETH = reserveData.priceInETH.div(1e18).toString();
+
+    reserveData.priceInUSD = new BigNumber(
+      reserveDataNonSpecific.priceInUSD.toString()
+    );
+    reserveData.priceInUSD = reserveData.priceInUSD.div(1e26).toString();
+
+    reserveData.supply = new BigNumber(reserveDataSpecific.supply.toString());
+    reserveData.supply = reserveData.supply.div(1e18).toString();
+
+    reserveData.borrow = new BigNumber(reserveDataSpecific.borrow.toString());
+    reserveData.borrow = reserveData.borrow.div(1e18).toString();
+
+    reserveData.maxBorrowLimit = new BigNumber(
+      reserveDataSpecific.maxBorrowLimit.toString()
+    );
+    reserveData.maxBorrowLimit = reserveData.maxBorrowLimit
+      .div(1e10)
+      .toString();
+
+    reserveData.maxLiquidationBorrowLimit = new BigNumber(
+      reserveDataSpecific.maxLiquidationBorrowLimit.toString()
+    );
+    reserveData.maxLiquidationBorrowLimit =
+      reserveData.maxLiquidationBorrowLimit.div(1e14).toString();
+
     console.log(reserveData);
   }
-  
-
-
 }
 
 GetUserPosition();
-
-function mod1ex(str, x) {
-  let len = str.length;
-  if (len <= x) {
-    let st0 = "0.";
-    for (let i = 0; i < x - len; i++) st0 += "0";
-    return st0 + str;
-  }
-  let beforedecimal = str.substring(0, len - x);
-  return beforedecimal + "." + str.substring(len - x);
-}
